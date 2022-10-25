30ccf431dd90e30239a434f4b8475444780d4531
Oli Hock <oliver.hock@gmail.com>
2022-10-25 22:14:26 +0200

    generate open api models and stubs

---
+++ particulate-matter/src/main/java/de/ithoc/particulatematter/ParticulateMatterApplication.java
@@ -0,0 +1,13 @@
+package de.ithoc.particulatematter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticulateMatterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParticulateMatterApplication.class, args);
+    }
+
+}
---
+++ particulate-matter/src/test/java/de/ithoc/particulatematter/ParticulateMatterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ithoc.particulatematter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParticulateMatterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
